e7531a5c8efc427c42976ca4f60d4288421a5720
alex tikonoff <tikonoff@alexs-MacBook-Pro.local>
2018-01-30 14:52:14 +1300

    more test added

--- audit/kovan-functional-tests.md
+++ audit/kovan-functional-tests.md
@@ -52,47 +52,56 @@ Tests are conducted on the Kovan test network, January 29 - 31, 2018, @tikonoff
 ## Expected behaviour tests
 by roles
 
+
 ### Owner
  - Deploy contracts:
- 	- [x] in the Future (Pre-Sale has not started yet), beneficiary is "Main Account" [0xf7d0c6](https://kovan.etherscan.io/tx/0xf7d0c68527f3341de0aa9958b9ec9923d803e25a36d5c9de2d0ac61fbd7500ce)
- 	- [x] PreSale period (NOW is today + 13 days), beneficiary is "Beneficiary account" [0xc02bc9](https://kovan.etherscan.io/tx/0xc02bc95ada5b7f0fd47b287f26c6a2d97c04fe111c6dcd92215b8ce2ed64af9a) 
+<!--	 - [x] SmartToken [0x9e8657](https://kovan.etherscan.io/tx/0x9e86576b9a8c6f4b3505bc8f04280a095cf5fc4aae31ecf7d40d6619f2c4f6e4)-->
+	 - [x] in the Future (Pre-Sale has not started yet), beneficiary is "Main Account" [0xf7d0c6](https://kovan.etherscan.io/tx/0xf7d0c68527f3341de0aa9958b9ec9923d803e25a36d5c9de2d0ac61fbd7500ce)
+	 - [x] PreSale period (NOW is today + 13 days), beneficiary is "Beneficiary account" [0xc02bc9](https://kovan.etherscan.io/tx/0xc02bc95ada5b7f0fd47b287f26c6a2d97c04fe111c6dcd92215b8ce2ed64af9a) 
  	- [x] for the Sale (NOW is January 30, 2018), beneficiary is "Beneficiary account" [0xd436be](https://kovan.etherscan.io/tx/0xd436be5c7d4c8230d2601da6252faa903751c57a8e5cd4a824f8fb3d2bbfb1f2) 
  	- [x] in the past (sale has over), beneficiary is "Beneficiary account"  - <font color="red">deployed with ERROR</font> [0x9902b9](https://kovan.etherscan.io/tx/0x9902b9a52cdb57ef1a94d091548af90417420eaba5b4dcaed69ec1247961e4ac)
  
 ### Contributors
- - [] ContributeETH []()
- - [] ContributePreSale - <font color=red>can't test, don't have 200ETH on Kovan</font>
- - [] PreSale period: GraceDuration is ON when SoftCap is reached
- - [] Sale period: GraceDuration is ON when SoftCap is reached
+ - [x] ContributeETH [0xfd7923](https://kovan.etherscan.io/tx/0xfd7923dafd1c5dba33768217bff7aec493502b622d16d7da345ec801adb119d8)
+
+ -  <font color=red>Can't test it NOW, due to prerequisites</font>
+	 - [] ContributePreSale - requires 200ETH
+	 - [] PreSale period: GraceDuration is ON when SoftCap is reached 
+	 - [] Sale period: GraceDuration is ON when SoftCap is reached
+	 - [] ContributePreSale when Soft MaxCap is reached []()
+	 - [] ContributePreSale when Hard MaxCap is reached []()
+	 - [] ContributeETH when Soft MaxCap is reached []()
+	 - [] ContributeETH when Hard MaxCap is reached []()
 
 
 ### Manager
  - [x] Transfer management [0x72b77d](https://kovan.etherscan.io/tx/0x72b77d80264760a3a84775b49a33a6be57a3aaab99f5e3fe4a693989c4fbdaa5)
  - [x] Accept management [0x5eee6b](https://kovan.etherscan.io/tx/0x5eee6ba00896bebcdc943bfb12f2f0ca48b3d455ceaa5c3a784a9e29eda61a20)
  - [x] Add account to WhiteList [0x4ed0a2](https://kovan.etherscan.io/tx/0x4ed0a25319f20ce4c1d289158595e676a231aef69cbd5b1c81a340f9b7b986fb)
+ - [x] Add to WhiteList already WhiteListed account [0x3b25d1](https://kovan.etherscan.io/tx/0x3b25d10613b43b694532ec84545806ff25605d585a1e0092e6b8614eef6c4b00)
  - [x] Remove account from WhiteList [0x4ee67f](https://kovan.etherscan.io/tx/0x4ee67fc4598e3d5614a5eaeb37568ab3a5a4375b7421c0a9c8bbea5c8b46f9aa)
  - [x] Pause Sale [0x91751f](https://kovan.etherscan.io/tx/0x91751f8edb026302164d4d29ac952642e746790c672cab249d440ca08aaeb8df)
  - [x] Unpause Sale [0x12f81d](https://kovan.etherscan.io/tx/0x12f81d578d3e76be43912cc9cdec958c2805372253c29057a7a0887cf1eb4bc3)
- - [] ContributeFiat  []()  - <b>CAN'T</b> Why? 
+ - [x] ContributeFiat  [0x78a745](https://kovan.etherscan.io/tx/0x78a745c16b27e843e2cd173a37c4dca31649ae9b698bb85945aa4fb4b21fa2d4)
  
 ### Token Owner
  - [] transfer() []()
  - [] transferFrom() []()
  - [] approve() []()
- - [] Enable / disable transfers []()
+
 
 ### Smart Token Contract controller
- - [] Enable / disable transfers []()
+ - [] Enable / disable transfers []() - <b>CAN'T</b> Why?
  - [] Issue token []()
  - [] Destroy tokens []()
- - [] Enable / disable transfers []()
+ - [] Enable / disable transfers []() 
  - [] Withdraw
 
 
 ### Contracts Owner
  - [] Transfer contracts' controller Ownership []()
- - [] Trying to accept Ownership []()
-
+ - [] Accepting Ownership []()
+ - [] Enable / disable tokens transfer []()
 
 ### System simulated events
  - [] Sale Soft cap 100.000 ETH has reached 
@@ -108,18 +117,14 @@ by roles
  - [] ContributePreSale when Sale is paused []()
  - [x] ContributePreSale with less than MIN_CONTRIBUTION (200ETH) [0xecd6d3](https://kovan.etherscan.io/tx/0xecd6d3d4535120d50dd8424e1917501646d59c0ec0683675d5aa7454e1132b0a)
  - [] ContributePreSale with gas price > 50 gWeis []()
- - [] ContributePreSale earlier than NOW() - 14 days []()
  - [] ContributePreSale later than NOW() []()
- - [] ContributePreSale from non-Whitelisted account []()
- - [] ContributePreSale when Soft MaxCap is reached []()
- - [] ContributePreSale when Hard MaxCap is reached []()
+ - [x] ContributePreSale from non-Whitelisted account [0xa451d7d](https://kovan.etherscan.io/tx/0xa451d7d9ebebebc4a407392b6b4e5633b7c5153bdb376d34b11876137364df23)
  - -
  - [] ContributeETH when Sale is paused []()
-I - [] ContributeETH with amount exceeding the limit []()
  - [] ContributeETH with gas price > 50 gWeis []()
  - [] ContributeETH later than NOW() + 14 days []()
- - [] ContributeETH earlier than NOW() []()
- - [] ContributeETH with amount exceeding MAX_CONTRIBUTION (40ETH) from non-Whitelisted account []()
+ - [] ContributeETH earlier than NOW() [0xa451d7](https://kovan.etherscan.io/tx/0xa451d7d9ebebebc4a407392b6b4e5633b7c5153bdb376d34b11876137364df23)
+ - [] ContributeETH with amount exceeding MAX_CONTRIBUTION (40ETH) from non-Whitelisted account []() 
  - [] ContributeETH when Soft MaxCap is reached []()
  - [] ContributeETH when Hard MaxCap is reached []()
 
@@ -134,6 +139,7 @@ I - [] ContributeETH with amount exceeding the limit []()
  - [] transferFrom() tokens from account with insufficient funds []()
  - [] approve() (should be called twice)[]()
  - [] Withdraw from .... after .... before ...
+ - [] Enable / disable transfers from unexpected account []() 
  - 
 
 ### Smart Token Contract controller
@@ -151,11 +157,11 @@ I - [] ContributeETH with amount exceeding the limit []()
 ### Manager
  - [] Transfer management from unexpected account []()
  - [x] Trying to accept management from unexpected account [0x3e2953](https://kovan.etherscan.io/tx/0x3e2953b6ac480b856036499cf88acf0de4401adf9a0f3ca601d710c39c4b9d46)
- - [] Transfer contract(s) Ownership from unexpected account []()
- - [] Trying to accept Ownership from unexpected account []()
- - [] Add non-existing account to WhiteList []()
- - [] Add to WhiteList already WhiteListed account []()
- - [] Remove non-existing account from WhiteList []()
+ - [x] Transfer contract(s) Ownership from unexpected account [0xfaeaed](https://kovan.etherscan.io/tx/0xfaeaede9bfdf89da95eb9371f8fa387d53f8ecc7c1e2e092960c66c2ef8d8408)
+ - [x] Trying to accept Ownership from unexpected account [0x1cb72d](https://kovan.etherscan.io/tx/0x1cb72d35a38f1ff1aa8627737b9d1ed250139848078d18de822c99042c69f17c)
+ - [x] Add non-existing account to WhiteList - <font color=coral>Can't - Parity doesn't allow.</font>
+ - [x] Add to WhiteList from unexpected account [0xfbf402](https://kovan.etherscan.io/tx/0xfbf402761ed4e1b6cb3d6efc18d0519cfdc0c4ecf83d4de675ae1474d2e770ed)
+ - [x] Remove non-existing account from WhiteList [0x149919a5](https://kovan.etherscan.io/tx/0x149919a5b8fc1bbdfc825c85a9431f3857a1776bd016fde876150033f96f5e4b)
  - [] Remove from WhiteList account which is not listed there[]()
  - [x] Pause already paused Sale [0xf9aac8](https://kovan.etherscan.io/tx/0xf9aac81be04e2496a9b0111f4014e432dd07e70fbb8686cead2c0fdd61fbff87)
  - [x] Unpause on-going Sale [0x6f1265](https://kovan.etherscan.io/tx/0x6f126548875e199ab2acc574aa3b6f39b5d8ae36e7ebb9c00746a2306d19f949)
---
+++ contracts/flattened/SmartTokenControllerFlattened.sol
@@ -0,0 +1,293 @@
+pragma solidity ^0.4.15;
+
+// File: contracts/interfaces/IOwned.sol
+
+/*
+    Owned contract interface
+*/
+contract IOwned {
+    // this function isn't abstract since the compiler emits automatically generated getter functions as external
+    function owner() public constant returns (address owner) { owner; }
+
+    function transferOwnership(address _newOwner) public;
+    function acceptOwnership() public;
+}
+
+// File: contracts/Owned.sol
+
+/*
+    Provides support and utilities for contract ownership
+*/
+contract Owned is IOwned {
+    address public owner;
+    address public newOwner;
+
+    event OwnerUpdate(address _prevOwner, address _newOwner);
+
+    /**
+        @dev constructor
+    */
+    function Owned() {
+        owner = msg.sender;
+    }
+
+    // allows execution by the owner only
+    modifier ownerOnly {
+        assert(msg.sender == owner);
+        _;
+    }
+
+    /**
+        @dev allows transferring the contract ownership
+        the new owner still needs to accept the transfer
+        can only be called by the contract owner
+
+        @param _newOwner    new contract owner
+    */
+    function transferOwnership(address _newOwner) public ownerOnly {
+        require(_newOwner != owner);
+        newOwner = _newOwner;
+    }
+
+    /**
+        @dev used by a new owner to accept an ownership transfer
+    */
+    function acceptOwnership() public {
+        require(msg.sender == newOwner);
+        OwnerUpdate(owner, newOwner);
+        owner = newOwner;
+        newOwner = 0x0;
+    }
+}
+
+// File: contracts/Utils.sol
+
+/*
+    Utilities & Common Modifiers
+*/
+contract Utils {
+    /**
+        constructor
+    */
+    function Utils() {
+    }
+
+    // verifies that an amount is greater than zero
+    modifier greaterThanZero(uint256 _amount) {
+        require(_amount > 0);
+        _;
+    }
+
+    // validates an address - currently only checks that it isn't null
+    modifier validAddress(address _address) {
+        require(_address != 0x0);
+        _;
+    }
+
+    // verifies that the address is different than this contract address
+    modifier notThis(address _address) {
+        require(_address != address(this));
+        _;
+    }
+
+    // Overflow protected math functions
+
+    /**
+        @dev returns the sum of _x and _y, asserts if the calculation overflows
+
+        @param _x   value 1
+        @param _y   value 2
+
+        @return sum
+    */
+    function safeAdd(uint256 _x, uint256 _y) internal returns (uint256) {
+        uint256 z = _x + _y;
+        assert(z >= _x);
+        return z;
+    }
+
+    /**
+        @dev returns the difference of _x minus _y, asserts if the subtraction results in a negative number
+
+        @param _x   minuend
+        @param _y   subtrahend
+
+        @return difference
+    */
+    function safeSub(uint256 _x, uint256 _y) internal returns (uint256) {
+        assert(_x >= _y);
+        return _x - _y;
+    }
+
+    /**
+        @dev returns the product of multiplying _x by _y, asserts if the calculation overflows
+
+        @param _x   factor 1
+        @param _y   factor 2
+
+        @return product
+    */
+    function safeMul(uint256 _x, uint256 _y) internal returns (uint256) {
+        uint256 z = _x * _y;
+        assert(_x == 0 || z / _x == _y);
+        return z;
+    }
+}
+
+// File: contracts/interfaces/IERC20Token.sol
+
+/*
+    ERC20 Standard Token interface
+*/
+contract IERC20Token {
+    // these functions aren't abstract since the compiler emits automatically generated getter functions as external
+    function name() public constant returns (string name) { name; }
+    function symbol() public constant returns (string symbol) { symbol; }
+    function decimals() public constant returns (uint8 decimals) { decimals; }
+    function totalSupply() public constant returns (uint256 totalSupply) { totalSupply; }
+    function balanceOf(address _owner) public constant returns (uint256 balance) { _owner; balance; }
+    function allowance(address _owner, address _spender) public constant returns (uint256 remaining) { _owner; _spender; remaining; }
+
+    function transfer(address _to, uint256 _value) public returns (bool success);
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success);
+    function approve(address _spender, uint256 _value) public returns (bool success);
+}
+
+// File: contracts/interfaces/ITokenHolder.sol
+
+/*
+    Token Holder interface
+*/
+contract ITokenHolder is IOwned {
+    function withdrawTokens(IERC20Token _token, address _to, uint256 _amount) public;
+}
+
+// File: contracts/TokenHolder.sol
+
+/*
+    We consider every contract to be a 'token holder' since it's currently not possible
+    for a contract to deny receiving tokens.
+
+    The TokenHolder's contract sole purpose is to provide a safety mechanism that allows
+    the owner to send tokens that were sent to the contract by mistake back to their sender.
+*/
+contract TokenHolder is ITokenHolder, Owned, Utils {
+    /**
+        @dev constructor
+    */
+    function TokenHolder() {
+    }
+
+    /**
+        @dev withdraws tokens held by the contract and sends them to an account
+        can only be called by the owner
+
+        @param _token   ERC20 token contract address
+        @param _to      account to receive the new amount
+        @param _amount  amount to withdraw
+    */
+    function withdrawTokens(IERC20Token _token, address _to, uint256 _amount)
+        public
+        ownerOnly
+        validAddress(_token)
+        validAddress(_to)
+        notThis(_to)
+    {
+        assert(_token.transfer(_to, _amount));
+    }
+}
+
+// File: contracts/interfaces/ISmartToken.sol
+
+/*
+    Smart Token interface
+*/
+contract ISmartToken is ITokenHolder, IERC20Token {
+    function disableTransfers(bool _disable) public;
+    function issue(address _to, uint256 _amount) public;
+    function destroy(address _from, uint256 _amount) public;
+}
+
+// File: contracts/SmartTokenController.sol
+
+/*
+    The smart token controller is an upgradable part of the smart token that allows
+    more functionality as well as fixes for bugs/exploits.
+    Once it accepts ownership of the token, it becomes the token's sole controller
+    that can execute any of its functions.
+
+    To upgrade the controller, ownership must be transferred to a new controller, along with
+    any relevant data.
+
+    The smart token must be set on construction and cannot be changed afterwards.
+    Wrappers are provided (as opposed to a single 'execute' function) for each of the token's functions, for easier access.
+
+    Note that the controller can transfer token ownership to a new controller that
+    doesn't allow executing any function on the token, for a trustless solution.
+    Doing that will also remove the owner's ability to upgrade the controller.
+*/
+contract SmartTokenController is TokenHolder {
+    ISmartToken public token;   // smart token
+
+    /**
+        @dev constructor
+    */
+    function SmartTokenController(ISmartToken _token)
+        validAddress(_token)
+    {
+        token = _token;
+    }
+
+    // ensures that the controller is the token's owner
+    modifier active() {
+        assert(token.owner() == address(this));
+        _;
+    }
+
+    // ensures that the controller is not the token's owner
+    modifier inactive() {
+        assert(token.owner() != address(this));
+        _;
+    }
+
+    /**
+        @dev allows transferring the token ownership
+        the new owner still need to accept the transfer
+        can only be called by the contract owner
+
+        @param _newOwner    new token owner
+    */
+    function transferTokenOwnership(address _newOwner) public ownerOnly {
+        token.transferOwnership(_newOwner);
+    }
+
+    /**
+        @dev used by a new owner to accept a token ownership transfer
+        can only be called by the contract owner
+    */
+    function acceptTokenOwnership() public ownerOnly {
+        token.acceptOwnership();
+    }
+
+    /**
+        @dev disables/enables token transfers
+        can only be called by the contract owner
+
+        @param _disable    true to disable transfers, false to enable them
+    */
+    function disableTokenTransfers(bool _disable) public ownerOnly {
+        token.disableTransfers(_disable);
+    }
+
+    /**
+        @dev withdraws tokens held by the token and sends them to an account
+        can only be called by the owner
+
+        @param _token   ERC20 token contract address
+        @param _to      account to receive the new amount
+        @param _amount  amount to withdraw
+    */
+    function withdrawFromToken(IERC20Token _token, address _to, uint256 _amount) public ownerOnly {
+        token.withdrawTokens(_token, _to, _amount);
+    }
+}
